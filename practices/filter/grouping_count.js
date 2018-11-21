b5252a53f66508b9780ed04f6384a42547532d40
HOJO3 <joe.ho@oocl.com>
2018-11-21 15:11:53 +0800

    [JoeHo] feat: Output Unique element from an array

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-const grouping_count = collection=>{
+const loopApproach = collection=>{
 	let dict = {}
 	let counted_array = []
 	
@@ -17,4 +17,19 @@ const grouping_count = collection=>{
 	return dict
 	
 }
+const filterMapApproach = () => {
+
+	//3. o: {'1':6, '2':3, '3':4, '4':2}
+}
+
+
+const grouping_count = collection=>{
+	let counts = {};
+	collection.forEach(x=> { counts[x] = (counts[x] || 0)+1; });
+	return counts;
+}
+const grouping_count = loopApproach
 module.exports = grouping_count;
+
+//1. i: [1,1,1,1,2,3,1,3,4,2,3,1,3,4,2]; o: [1,2,3,4]
+const findUniqueElement = collection => {return [...new Set(collection)]} 
