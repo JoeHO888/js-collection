03b97592fca250c04f2c8a6b930e3b929a4339a4
HOJO3 <joe.ho@oocl.com>
2018-11-21 16:25:38 +0800

    [JoeHo] feat: create CreateMapping and CreateDict

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,6 +1,38 @@
 'use strict';
 
 
+//1. i: [1,1,1,1,2,3,1,3,4,2,3,1,3,4,2]; o: [1,2,3,4]
+const findUniqueElement = collection => {return [...new Set(collection)]}
+
+
+//2. i: [1,1,1,1,2,3,1,3,4,2,3,1,3,4,2],[1,2,3,4]; o: [6,3,4,2]
+const CreateMapping = (collection,UniqueElement) => {
+	return UniqueElement.map(e=>collection.filter(l=>l==e).length)
+}
+
+
+//3. i:[1,2,3,4],[6,3,4,2] ;o: {'1':6, '2':3, '3':4, '4':2}
+const CreateDict = (UniqueElement,count) => {
+	let dict ={}
+	
+	UniqueElement.map((e,index)=>dict[e]=count[index])
+	return dict
+}
+
+
+const grouping_count = collection=>CreateDict(findUniqueElement(collection),CreateMapping(collection,findUniqueElement(collection)))
+module.exports = grouping_count;
+
+
+
+/*
+const grouping_count = collection=>{
+	let counts = {};
+	collection.forEach(x=> { counts[x] = (counts[x] || 0)+1; });
+	return counts;
+}
+*/
+/*
 const loopApproach = collection=>{
 	let dict = {}
 	let counted_array = []
@@ -17,19 +49,7 @@ const loopApproach = collection=>{
 	return dict
 	
 }
-const filterMapApproach = () => {
-
-	//3. o: {'1':6, '2':3, '3':4, '4':2}
-}
+*/
 
 
-const grouping_count = collection=>{
-	let counts = {};
-	collection.forEach(x=> { counts[x] = (counts[x] || 0)+1; });
-	return counts;
-}
-const grouping_count = loopApproach
-module.exports = grouping_count;
-
-//1. i: [1,1,1,1,2,3,1,3,4,2,3,1,3,4,2]; o: [1,2,3,4]
-const findUniqueElement = collection => {return [...new Set(collection)]} 
+ 
